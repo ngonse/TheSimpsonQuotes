5c0a692653858ca82b65b75b2990ae10d37fdb09
Makkpas <axis12@gmail.com>
2020-10-06 22:16:54 -0600

    Primer Commit

---
+++ src/util/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const instance = axios.create({
+	baseURL: 'https://thesimpsonsquoteapi.glitch.me/',
+	headers: { 'Content-Type': 'application/json' },
+});
+
+
+export default instance;
+
